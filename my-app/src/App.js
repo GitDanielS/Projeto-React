bb13c57e44b69847ef9dfe4f0838a7fda3fcd5be
Daniel123457 <128873792+Daniel123457@users.noreply.github.com>
2024-01-07 10:50:21 -0300

    Atualizando

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import PrimeiroComponente from './componentes/PrimeiroComponente'
-import BoxG from './componentes/BoxG'
-import MediaRecord from './componentes/MediaRecord'
+import MediaRecord from './componentes/MediaRecord'//Aqui estou importando o componente
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* Aqui estou adicionando o componente */}
         <MediaRecord />
-        <BoxG />
-        <PrimeiroComponente />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- my-app/src/componentes/MediaRecord.js
+++ my-app/src/componentes/MediaRecord.js
@@ -1,32 +1,38 @@
 import React, { useState } from 'react';
 
 const Gravador = function() {
+  // Estados para armazenar a transcrição, estado de gravação e objeto de reconhecimento
   const [transcricao, setTranscricao] = useState('');
   const [estaGravando, setEstaGravando] = useState(false);
   const [reconhecimento, setReconhecimento] = useState(null);
   var transcript = "";
 
+  // Função para iniciar a gravação
   const iniciarGravacao = async function() {
     setEstaGravando(true);
 
+    // Cria uma nova instância de reconhecimento de fala
     const recognition = new window.webkitSpeechRecognition();
 
+    // Evento chamado quando há resultados na transcrição
     recognition.onresult = function(event) {
       transcript = event.results[0][0].transcript;
       setTranscricao(transcript);
     };
 
+    // Evento chamado quando a gravação é encerrada
     recognition.onend = function() {
       // Aqui vai limitar a gravação para não ficar muito grande
       setTimeout(() => {
         setEstaGravando(false);
-      }, 1800);  
+      }, 1800);
     };
 
     setReconhecimento(recognition);
     recognition.start();
   };
 
+  // Função para pausar a gravação
   const pausarGravacao = function() {
     if (reconhecimento) {
       reconhecimento.stop();
@@ -36,34 +42,26 @@ const Gravador = function() {
 
   return (
     <>
+      {/* Botões para iniciar ou pausar a gravação */}
       <div className="flex">
         {estaGravando ? (
-          <button onClick={pausarGravacao} className="m-6 w-[200px] h-[100px] bg-red-700 rounded-3xl">
-            <img src="https://www.svgrepo.com/show/364660/microphone-slash-fill.svg" alt="microfone"/>
+          <button onClick={pausarGravacao} className="m-6 w-[150px] h-[100px] bg-red-700 rounded-3xl mt-20">
+            <img src="https://www.svgrepo.com/show/364660/microphone-slash-fill.svg" className="h-[90%] mx-auto" alt="microfone"/>
           </button>
         ) : (
-          <button onClick={iniciarGravacao} className="m-6 w-[200px] h-[100px] bg-red-700 rounded-3xl">
-            <img src="https://www.svgrepo.com/show/364659/microphone-fill.svg" alt="microfone"/>
+          <button onClick={iniciarGravacao} className="m-6 w-[150px] h-[100px] bg-red-700 rounded-3xl mt-20">
+            <img src="https://www.svgrepo.com/show/364659/microphone-fill.svg" className="h-[90%] mx-auto" alt="microfone"/>
           </button>
         )}
-        
-        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-          Your message
-        </label>
+
+        {/* Área de texto para exibir a transcrição */}
         <textarea
           id="message"
-          rows="4"
           value={transcricao}
-          onChange={(e) => setTranscricao(e.target.value)}  // Atualize sempre quando estiver fazendo uma nova gravação
-          className="text-black block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          onChange={(e) => setTranscricao(e.target.value)}
+          className="resize-none w-[500px] h-[300px] text-black block p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           readOnly
         ></textarea>
-      </div>
-
-      <div className="grid grid-cols-3 gap-4">
-        <div className="w-[300px] h-[300px] bg-blue-300"></div>
-        <div className="w-[300px] h-[300px] bg-blue-300"></div>
-        <div className="w-[300px] h-[300px] bg-blue-300"></div>
       </div>
     </>
   );
