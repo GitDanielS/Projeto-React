34cba30e2dea535478a72fa481c073fe8dbca962
Daniel123457 <128873792+Daniel123457@users.noreply.github.com>
2024-01-07 10:53:09 -0300

    Atualizando

--- my-app/src/componentes/MediaRecord.js
+++ my-app/src/componentes/MediaRecord.js
@@ -58,7 +58,7 @@ const Gravador = function() {
         <textarea
           id="message"
           value={transcricao}
-          onChange={(e) => setTranscricao(e.target.value)}
+          onChange={(e) => setTranscricao(e.target.value)}//Atualiza sempre quando iniciar uma nova gravação
           className="resize-none w-[500px] h-[300px] text-black block p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           readOnly
         ></textarea>
